214a135a41625b96a26321ddfd9cf1fc814de543
mskold <markus.skold@kb.se>
2015-06-02 16:04:50 +0200

    store with version

--- lddb/storage.py
+++ lddb/storage.py
@@ -2,47 +2,100 @@
 
 import psycopg2
 import json
+import hashlib
 from datetime import datetime
+import collections
 
 class Storage:
+
     def __init__(self, base_table, database, host, user, password):
         self.connection = psycopg2.connect(database=database, user=user, host=host, password=password)
         self.tname = base_table
+        self.vtname = "{0}__versions".format(base_table)
+        self.versioning = True
 
+    # Load-methods
     def load(self, identifier):
         """Returns a tuple containing the records identifier, data and entry."""
         cursor = self.connection.cursor()
-        cursor.execute("SELECT identifier,data,entry FROM {table} WHERE identifier = '{identifier}'".format(table=self.tname, identifier=identifier))
+        cursor.execute("SELECT id,data,entry FROM {table} WHERE id = '{identifier}'".format(table=self.tname, identifier=identifier))
         result = cursor.fetchone()
+        self.connection.commit()
         if result:
             return (result[0], result[1], result[2])
         return None
 
-    def _assemble_thing_list(self, results):
+    def _assemble_result_list(self, results):
         for result in results:
             yield (result[0], result[1], result[2])
 
     def load_thing(self, identifier):
         """Finds record(s) decribing a thing. Returns a list of tuples containing identifier, data and entry."""
         cursor = self.connection.cursor()
-        sql = "SELECT identifier,data,entry FROM "+self.tname+" WHERE data #> '{graph,@id}' = %(identifier)s"
-        cursor.execute(sql, {'identifier': '"%s"' % identifier})
-        return list(self._assemble_thing_list(cursor))
+        query_dict = {
+            "@graph": [
+                { "@id": identifier }
+            ]
+        }
+
+        print("json", json.dumps(query_dict))
+        sql = "SELECT id,data,entry FROM "+self.tname+" WHERE data @> %(json)s"
+        cursor.execute(sql, {'json': json.dumps(query_dict)})
+        result = list(self._assemble_result_list(cursor))
+        self.connection.commit()
+        return result
+
+    def load_all_versions(self, identifier):
+        cursor = self.connection.cursor()
+        if self.versioning:
+            sql = "SELECT id,data,entry FROM "+self.vtname+" WHERE id = %{identifier}s ORDER BY ts ASC"
+            cursor.execute(sql, {'identifier': identifier})
+            result = list(self._assemble_result_list(cursor))
+            self.connection.commit()
+        else:
+            result = load(identifier)
+        return result
+
+
+    # Store methods
+    def _calc_checksum(self, data):
+        md5 = hashlib.md5()
+        ordered_data = collections.OrderedDict(sorted(data.items()))
+        md5.update(json.dumps(ordered_data).encode('utf-8'))
+        return md5.hexdigest()
 
     def store(self, identifier, data, entry):
-        cursor = self.connection.cursor()
-        sql = """WITH upsert AS (UPDATE {table_name} SET data = %(data)s, ts = %(ts)s, entry = %(entry)s, deleted = %(deleted)s
-            WHERE identifier = %(identifier)s RETURNING *)
-            INSERT INTO {table_name} (identifier, data, ts, entry, deleted) SELECT %(identifier)s, %(data)s, %(ts)s, %(entry)s, %(deleted)s
-            WHERE NOT EXISTS (SELECT * FROM upsert)""".format(table_name = self.tname)
-        cursor.execute(sql, {
-                'identifier': identifier,
-                'data': json.dumps(data),
-                'entry': json.dumps(entry),
-                'ts': datetime.fromtimestamp(entry['modified']),
-                'deleted': entry.get('deleted', False),
-            }
-        )
+        try:
+            cursor = self.connection.cursor()
+            entry['checksum'] = self._calc_checksum(data)
+            if self.versioning:
+                insert_version_sql = "INSERT INTO {version_table_name} (id,checksum,data,entry,ts) SELECT %(identifier)s,%(checksum)s,%(data)s,%(entry)s,%(ts)s WHERE NOT EXISTS (SELECT 1 FROM {version_table_name} WHERE id = %(identifier)s AND checksum = %(checksum)s)".format(version_table_name = self.vtname)
+                cursor.execute(insert_version_sql, {
+                        'identifier': identifier,
+                        'entry': json.dumps(entry),
+                        'data': json.dumps(data),
+                        'checksum': entry['checksum'],
+                        'ts': datetime.fromtimestamp(entry['modified']),
+                    }
+                )
 
-        self.connection.commit()
-        cursor.close()
+            upsert = """WITH upsert AS (UPDATE {table_name} SET data = %(data)s, ts = %(ts)s, entry = %(entry)s, deleted = %(deleted)s
+                WHERE id = %(identifier)s RETURNING *)
+                INSERT INTO {table_name} (id, data, ts, entry, deleted) SELECT %(identifier)s, %(data)s, %(ts)s, %(entry)s, %(deleted)s
+                WHERE NOT EXISTS (SELECT * FROM upsert)""".format(table_name = self.tname)
+            cursor.execute(upsert, {
+                    'identifier': identifier,
+                    'data': json.dumps(data),
+                    'entry': json.dumps(entry),
+                    'ts': datetime.fromtimestamp(entry['modified']),
+                    'deleted': entry.get('deleted', False),
+                }
+            )
+
+            self.connection.commit()
+        except Exception as e:
+            print("Store failed. Rolling back.", e)
+            self.connection.rollback()
+            raise e
+
+
