a19e6e2b59c07020661da0c4dfbf94ebbb5cebcd
mskold <markus.skold@kb.se>
2015-11-17 12:43:24 +0100

    added settings table

--- lddb/config/tables.sql
+++ lddb/config/tables.sql
@@ -17,3 +17,8 @@ CREATE TABLE IF NOT EXISTS lddb__versions (
     modified timestamp with time zone not null default now(),
     unique (id, checksum, modified)
     );
+
+CREATE TABLE IF NOT EXISTS lddb__settings (
+    key text not null unique primary key,
+    settings jsonb not null
+);
